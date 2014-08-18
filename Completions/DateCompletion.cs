0d529fe93b878cb843e6154ea1510552a1a1a8ac
Andy Sherwood <xinmyname@gmail.com>
2014-08-17 19:45:45 -0700

    date completion for months

--- Completions/DateCompletion.cs
+++ Completions/DateCompletion.cs
@@ -9,5 +9,20 @@ namespace SWPCCBilling2.Completions
 
 	public class DateCompletion : FilteredStringCompletion
 	{
+		public DateCompletion()
+		{
+			AllStrings.Add("January");
+			AllStrings.Add("February");
+			AllStrings.Add("March");
+			AllStrings.Add("April");
+			AllStrings.Add("May");
+			AllStrings.Add("June");
+			AllStrings.Add("July");
+			AllStrings.Add("August");
+			AllStrings.Add("September");
+			AllStrings.Add("October");
+			AllStrings.Add("November");
+			AllStrings.Add("December");
+		}
 	}
 }
--- Infrastructure/CommandLineFactory.cs
+++ Infrastructure/CommandLineFactory.cs
@@ -413,11 +413,56 @@ namespace SWPCCBilling2.Infrastructure
 				Span paramSpan = paramSpans[i];
 				ActionParam actionParam = actionParams[i];
 
-				object parameter = Convert.ChangeType(paramSpan.Text, actionParam.ParamType);
+				object parameter;
+				if (actionParam.ParamType == typeof(DateTime) || actionParam.ParamType == typeof(DateTime?))
+					parameter = DateFromText(paramSpan.Text);
+				else
+					parameter = Convert.ChangeType(paramSpan.Text, actionParam.ParamType);
+
 				parameters.Add(parameter);
 			}
 
 			return parameters.ToArray();
+		}
+
+		public static DateTime DateFromText(string text)
+		{
+			DateTime now = DateTime.Now;
+
+			switch (text.ToLower().Substring(0, 3))
+			{
+				case "jan": return MonthClosestTo(now, 1);
+				case "feb": return MonthClosestTo(now, 2);
+				case "mar": return MonthClosestTo(now, 3);
+				case "apr": return MonthClosestTo(now, 4);
+				case "may": return MonthClosestTo(now, 5);
+				case "jun": return MonthClosestTo(now, 6);
+				case "jul": return MonthClosestTo(now, 7);
+				case "aug": return MonthClosestTo(now, 8);
+				case "sep": return MonthClosestTo(now, 9);
+				case "oct": return MonthClosestTo(now, 10);
+				case "nov": return MonthClosestTo(now, 11);
+				case "dec": return MonthClosestTo(now, 12);
+			}
+
+			return Convert.ToDateTime(text);
+		}
+
+		public static DateTime MonthClosestTo(DateTime date, int month)
+		{
+			DateTime lastYear = new DateTime(date.Year - 1, month, 1);
+			DateTime thisYear = new DateTime(date.Year, month, 1);
+			DateTime nextYear = new DateTime(date.Year + 1, month, 1);
+
+			var deltas = new Dictionary<long, DateTime>();
+
+			deltas[Math.Abs((lastYear - date).Ticks)] = lastYear;
+			deltas[Math.Abs((thisYear - date).Ticks)] = thisYear;
+			deltas[Math.Abs((nextYear - date).Ticks)] = nextYear;
+
+			long smallestDelta = deltas.Keys.Min();
+
+			return deltas[smallestDelta];
 		}
 	}
 }
