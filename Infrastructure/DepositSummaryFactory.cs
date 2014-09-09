7a7dce5d4416c2016e1e25396ed0bc7e72da690d
Andy Sherwood <xinmyname@gmail.com>
2014-09-08 22:29:03 -0700

    Maybe it works

--- Infrastructure/DepositSummaryFactory.cs
+++ Infrastructure/DepositSummaryFactory.cs
@@ -11,8 +11,21 @@ namespace SWPCCBilling2.Infrastructure
 
 	public class DepositSummaryFactory
 	{
+		private readonly IDictionary<Deposit, IDictionary<string, double>> _deposits;
+		private readonly HashSet<string> _headers;
+
+		public DepositSummaryFactory()
+		{
+			_deposits = new Dictionary<Deposit, IDictionary<string, double>>();
+			_headers = new HashSet<string>();
+		}
+
 		public void AddDeposit(Deposit deposit, IDictionary<string, double> categoryTotals)
 		{
+			_deposits[deposit] = categoryTotals;
+
+			foreach (string category in categoryTotals.Keys)
+				_headers.Add(category);
 		}
 
 		public IEnumerable<string> BuildHeaderRow()
@@ -20,35 +33,52 @@ namespace SWPCCBilling2.Infrastructure
 			yield return @"<tr>";
 			yield return @"<th>Deposit</th>";
 			yield return @"<th>Date</th>";
-			yield return @"<th class=""money"">Tuition</th>";
-			yield return @"<th class=""money"">Insurance</th>";
-			yield return @"<th class=""money"">Credits</th>";
+
+			foreach (string header in _headers.OrderByDescending(h => h))
+				yield return String.Format(@"<th class=""money"">{0}</th>", header);
+	
 			yield return @"<th class=""money"">Total</th>";
 			yield return @"</tr>";
 		}
 
 		public IEnumerable<string> BuildDepositRows()
 		{
-			yield return @"<tr>";
-			yield return @"<td>1</td>";
-			yield return @"<td>1/1/2010</td>";
-			yield return @"<td class=""money"">$4000.00</td>";
-			yield return @"<td class=""money"">$500.00</td>";
-			yield return @"<td class=""money"">$0.00</td>";
-			yield return @"<td class=""money"">$4500.00</td>";
-			yield return @"</tr>";
-			yield return @"<tr>";
-			yield return @"<td>2</td>";
-			yield return @"<td>1/2/2010</td>";
-			yield return @"<td class=""money"">$3000.00</td>";
-			yield return @"<td class=""money"">$200.00</td>";
-			yield return @"<td class=""money"">$0.00</td>";
-			yield return @"<td class=""money"">$3200.00</td>";
-			yield return @"</tr>";
+			IList<string> headers = _headers.OrderByDescending(h => h).ToList();
+			IList<Deposit> deposits = _deposits.Keys.OrderBy(d => d.Date).ToList();
+
+			foreach (Deposit deposit in deposits)
+			{
+				IDictionary<string, double> categoryTotals = _deposits[deposit];
+
+				yield return @"<tr>";
+				yield return String.Format(@"<td>{0}</td>", deposit.Id);
+				yield return String.Format(@"<td>{0:d}</td>", deposit.Date);
+
+				decimal total = 0;
+
+				foreach (string header in headers)
+				{
+					decimal value = 0;
+
+					if (categoryTotals.ContainsKey(header))
+					{
+						value = (decimal)categoryTotals[header];
+						total += value;
+					}
+
+					yield return String.Format(@"<td class=""money"">{0}</td>", value.ToHtmlCurrency());
+				}
+
+				yield return String.Format(@"<td class=""money"">{0}</td>", total.ToHtmlCurrency());
+
+				yield return @"</tr>";
+			}
 		}
 
 		public void Clear()
 		{
+			_deposits.Clear();
+			_headers.Clear();
 		}
 	}
 }
